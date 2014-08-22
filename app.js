9105095fdac81c2aff7c018f4b85b053e7c7c212
Buttreygoodness <jeff@readypress.com>
2014-08-22 11:10:24 -0700

    Initial app.

---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 8888;
+
+var getContent = function(url, callback) {
+  var content = '';
+  // Here we spawn a phantom.js process, the first element of the 
+  // array is our phantomjs script and the second element is our url 
+  var phantom = require('child_process').spawn('phantomjs', ['phantom-server.js', url, '--load-images=false', '--disk-cache=true', 'local-storage-path=./cache']);
+  phantom.stdout.setEncoding('utf8');
+  // Our phantom.js script is simply logging the output and
+  // we access it here through stdout
+  phantom.stdout.on('data', function(data) {
+    content += data.toString();
+  });
+  phantom.on('exit', function(code) {
+    if (code !== 0) {
+      console.log('We have an error');
+    } else {
+      // once our phantom.js script exits, let's call out call back
+      // which outputs the contents to the page
+      callback(content);
+    }
+  });
+};
+
+var respond = function (req, res) {
+  // Because we use [P] in htaccess we have access to this header
+  url = 'http://' + process.env.webhost + req.params[0];
+  getContent(url, function (content) {
+    res.send(content);
+  });
+}
+
+app.get(/(.*)/, respond);
+app.listen(PORT);
