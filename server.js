ccf95fbdd31c1ccfc30550d6418d958bd1bc2b50
Buttreygoodness <jeff@readypress.com>
2014-08-23 13:15:40 -0700

    Trying to get around serving anything bu the rendered page.

--- server.js
+++ server.js
@@ -1,20 +1,39 @@
 var express = require('express');
+var request = require('request');
 var app = express();
 var PORT = process.env.PORT || 8888;
 
+var sendContent = function (url, res) {
+
+  if (/\.js/.test(url)) {
+    res.send('var fake=true;');
+    return;
+  }
+
+  request(url, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.send(body);
+    }
+  });
+
+};
+
 var getContent = function(url, callback) {
+  console.log('Getting content for ' + url);
 
   var content = '';
   // Here we spawn a phantom.js process, the first element of the 
   // array is our phantomjs script and the second element is our url 
-  var phantom = require('child_process').spawn('phantomjs', ['phantom-server.js', url]);
+  var phantom = require('child_process').spawn('phantomjs', ['phantom-server.js', url, '--config=' + process.cwd() + 'config.json']);
 
   phantom.stdout.setEncoding('utf8');
+
   // Our phantom.js script is simply logging the output and
   // we access it here through stdout
   phantom.stdout.on('data', function(data) {
     content += data.toString();
   });
+
   phantom.on('exit', function(code) {
     if (code !== 0) {
       console.log('We have an error');
@@ -28,8 +47,12 @@ var getContent = function(url, callback) {
 
 var respond = function (req, res) {
   var webhost = process.env.webhost || 'local.host:3000'; 
-  // Because we use [P] in htaccess we have access to this header
   url = 'http://' + webhost + req.params[0];
+
+  if (/^\/assets\/.*/.test(req.params[0])) {
+    return sendContent(url, res);
+  }
+
   getContent(url, function (content) {
     res.send(content);
   });
