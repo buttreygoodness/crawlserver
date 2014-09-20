fb04313c7d60cc36ff373bdc08962cdd30693c96
Buttreygoodness <jeff@readypress.com>
2014-09-19 17:17:14 -0700

    Using environment variables.

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 Crawlserver = require('crawlserver');
+PORT = process.env.PORT || 8888;
+WEBHOST = process.env.WEBHOST || "http://0.0.0.0:3000";
 
-cs = new Crawlserver();
+cs = new Crawlserver({
+  "port": PORT,
+  "transport from": WEBHOST
+});
 
-cs.listen();
+cs.listen(PORT);
